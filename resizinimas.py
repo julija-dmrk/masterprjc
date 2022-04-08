f418ae13b2455c471c19fdf2c2c671bff4210b57
julija-dmrk <74661437+julija-dmrk@users.noreply.github.com>
2022-04-08 13:01:59 +0300

    updt resizing
    Created a formula to generate resized images for images, ground truths and masks

--- resizinimas.py
+++ resizinimas.py
@@ -5,44 +5,74 @@ import matplotlib.pyplot as plt
 from PIL import Image
 from numpy import asarray
 
-mask_train = "./DRIVE/training/mask/"
+img_path = './all_data/laikinai/cropped_img/'
+grt_path = './all_data/laikinai/cropped_grt/'
+msk_path = './all_data/laikinai/cropped_msk/'
 
-for image in os.listdir(mask_train):
-    if image.endswith('.gif'):
-        im = Image.open(mask_train + image).convert('RGB')
+#--- DEFINE SIZE---:
+new_size = (565, 565)
+
+def resize_data(imgs_dir, groundTruth_dir, borderMasks_dir):
+    files = os.listdir(imgs_dir)
+    assert len(files) > 0
+    img = cv2.imread(imgs_dir + files[0])
+    for i in range(len(files)):
+    # original
+        print("original image: " + files[i])
+    # corresponding ground truth
+        groundTruth_name = files[i][0:5] + '_grt_' + files[i][10:13] + '_sqr.tif'
+        print("ground truth name: " + groundTruth_name)
+        border_masks_name = files[i][0:5] + '_msk_' + files[i][10:13] + '_sqr.tif'
+        print("border masks name: " + border_masks_name)
+        print('Resizing images')
+    # Resizing images
+        im = cv2.imread(imgs_dir + files[i])
         im_c = im.copy()
-        im_c = asarray(im_c)
-        gray_scale = cv2.cvtColor(im_c, cv2.COLOR_BGR2GRAY)
-        #
-        _, thresh = cv2.threshold(gray_scale,20, 255, cv2.THRESH_BINARY)
-        thresh = thresh/255.
-        # plt.imshow(thresh)
-        # plt.show()
-        #
-        thresh1 = thresh.astype(np.uint8)
+    # - NEAREST NEIGHBOUR -#
+        new_im = cv2.resize(im_c,new_size, interpolation =cv2.INTER_NEAREST)
+        basename = os.path.basename(files[i])  # e.g. MyPhoto.jpg
+        name = os.path.splitext(basename)[0]
+        print("saving " + files[i] + " near_neigh")
+        cv2.imwrite('./all_data/train/images/sqr/565_565/neigh/'+ name + '_neigh.tif' , new_im)
+    # - LINEAR -#
+        new_im = cv2.resize(im_c, new_size, interpolation=cv2.INTER_LINEAR)
+        print("saving " + files[i] + " linear")
+        cv2.imwrite('./all_data/train/images/sqr/565_565/linear/' + name + '_liner.tif', new_im)
+    # - BICUBIC -#
+        new_im = cv2.resize(im_c, new_size, interpolation=cv2.INTER_CUBIC)
+        print("saving " + files[i] + " bicubic")
+        cv2.imwrite('./all_data/train/images/sqr/565_565/cubic/' + name + '_cubic.tif', new_im)
 
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
-        morphed = cv2.morphologyEx(thresh1, cv2.MORPH_CLOSE, kernel)
+        ### all same to grt
+        print('Resizing ground truths')
+        g_truth = cv2.imread(groundTruth_dir + groundTruth_name)
+        g = g_truth.copy()
+        new_im = cv2.resize(g, new_size, interpolation=cv2.INTER_NEAREST)
+        basename = os.path.basename(groundTruth_name)  # e.g. MyPhoto.jpg
+        name = os.path.splitext(basename)[0]
+        print("saving " + groundTruth_name + " near_neigh")
+        cv2.imwrite('./all_data/train/manual/sqr/565_565/neigh/' + name + '_neigh.tif', new_im)
+        new_im = cv2.resize(g, new_size, interpolation=cv2.INTER_LINEAR)
+        print("saving " + groundTruth_name + " linear")
+        cv2.imwrite('./all_data/train/manual/sqr/565_565/linear/' + name + '_liner.tif', new_im)
+        new_im = cv2.resize(g, new_size, interpolation=cv2.INTER_CUBIC)
+        print("saving " + groundTruth_name + " bicubic")
+        cv2.imwrite('./all_data/train/manual/sqr/565_565/cubic/' + name + '_cubic.tif', new_im)
 
-        ##LEAVING ONLY FOV
-        ## the max-area contour
-        cnts = cv2.findContours(morphed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-        cnt = sorted(cnts, key=cv2.contourArea)[-1]
+        ### all same to msks
+        print('Resizing border masks')
+        b_mask = cv2.imread(borderMasks_dir + border_masks_name)
+        b = b_mask.copy()
+        new_im = cv2.resize(b, new_size, interpolation=cv2.INTER_NEAREST)
+        basename = os.path.basename(border_masks_name)  # e.g. MyPhoto.jpg
+        name = os.path.splitext(basename)[0]
+        print("saving " + border_masks_name + " near_neigh")
+        cv2.imwrite('./all_data/train/masks/sqr/565_565/neigh/' + name + '_neigh.tif', new_im)
+        new_im = cv2.resize(b, new_size, interpolation=cv2.INTER_LINEAR)
+        print("saving " + border_masks_name + " linear")
+        cv2.imwrite('./all_data/train/masks/sqr/565_565/linear/' + name + '_liner.tif', new_im)
+        new_im = cv2.resize(b, new_size, interpolation=cv2.INTER_CUBIC)
+        print("saving " + border_masks_name + " bicubic")
+        cv2.imwrite('./all_data/train/masks/sqr/565_565/cubic/' + name + '_cubic.tif', new_im)
 
-        ## Crop and save it
-        x, y, w, h = cv2.boundingRect(cnt)
-        dst = im_c[y:y + h, x:x + w]
-        # plt.imshow(dst)
-        # plt.show()
-        #
-        basename = os.path.basename(image)  # e.g. MyPhoto.jpg
-        name = os.path.splitext(basename)[0]  # e.g. MyPhoto
-        cv2.imwrite('./DRIVE/training/mask/cropped/' + name[0:2] + '_drive_msk_sqr.tif', dst)
-
-
-        new_size = (2260,2260)
-        print(im_c.shape)
-        print(dst.shape)
-        new_im = cv2.resize(dst,new_size, interpolation =cv2.INTER_CUBIC)
-        #new_name = os.path.join('./DRHAGIS/test/images/cropped/565x584/', "sized_" + image)
-        cv2.imwrite('./DRIVE/training/mask/cropped/2260x2260/' + name[0:2] + '_drive_msk_sqr2_r_cubic.tif', new_im)
+resize_data(img_path, grt_path, msk_path)
